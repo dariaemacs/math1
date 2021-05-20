2dd8bdda3be1f2ffb30b3df1c34a207d3649c910
Andrew Gurenkov <andron81_81@mail.ru>
2021-05-20 16:20:43 +0300

    'almost_ok'

--- include/database.hpp
+++ include/database.hpp
@@ -13,9 +13,14 @@ struct figureQestions {
 
 extern const std::wstring CommentsDic[];
 
-constexpr int N = 9; //Question count
+constexpr int N = 9; 
+constexpr int X = 6; 
+constexpr int Y = 5;
+
 const int ELAPSED_TIME = 20;
 const float FPS = 30.0f;
+
+
 
 
 extern const std::array<figureQestions, N> questionFigure;
@@ -23,6 +28,17 @@ extern const std::array<figureQestions, N> questionFigure;
 extern const std::vector<pica> tree;
 extern const std::vector<std::string> iconsFilename;
 extern const std::vector<FrameFigure*> figures;
+
+struct prasetoReplace {
+	std::wstring find;
+	std::wstring replace;
+};
+
+//prasetoReplace OOO[] = { { L"SDF",L"cvcv" },{ L"SDF",L"cvcv" } };
+//const std::array <std::array<int, X>, Y> INT0 = { { L"SDF",L"cvcv" },{ L"SDF",L"cvcv" } };
+
+extern const std::array <std::array<prasetoReplace, X>,Y> aaa;
+//extern const std::array <std::array<int, X>, Y> INT0;
 
 #endif //DATABASE_H
 
--- src/database.cpp
+++ src/database.cpp
@@ -8,16 +8,79 @@ const std::array<figureQestions,N> questionFigure =
     figureQestions{L"Сколько кружков на рисунках вы видете ? "       ,circle},
     figureQestions{L"Сколько прямоугольников на рисунках вы видите ? ",rectangle},
     figureQestions{L"Сколько треугольников на рисунках вы видите ? ",triangle},
+
     figureQestions{L"Лена хочет подарить родным N ракушек. Ей осталось \nнайти M ракушек . Сколько ракушек Лена уже нашла ?",seashell},
     figureQestions{L"В корзине было N ягод клубники. После того , как Игорь съел \nнесколько ягод, осталось M. Сколько ягод съел Игорь ?",berry},
     figureQestions{L"Ване нужно найти N грибов. Ему осталось найти M грибов. \nСколько грибов он уже нашел ?",mashroom},
-    figureQestions{L"Паша и Люба поймали 18 рыбок. Паша поймал 14 рыбок. \nСколько рыбок поймала Люба?",fish},
-    figureQestions{L"Наташе нужно раскрасить 20 бабочек. Она раскрасила \n14 бабочек. Сколько бабочек осталось раскрасить? ",butterfly},
-    figureQestions{L"Саша хочет купить велосипед. Он стоит 16 рублей, \nсейчас у Саши в копилке 9 рублей. Сколько  еще рублей нужно Саше, \nчтобы хватило на велосипед?",bycicle}
+    figureQestions{L"Паша и Люба поймали N рыбок. Паша поймал M рыбок. \nСколько рыбок поймала Люба?",fish},
+    figureQestions{L"Наташе нужно раскрасить N бабочек. Она раскрасила \nN бабочек. Сколько бабочек осталось раскрасить? ",butterfly},
+    figureQestions{L"Саша хочет купить велосипед. Он стоит N рублей, \nсейчас у Саши в копилке M рублей. Сколько  еще рублей нужно Саше, \nчтобы хватило на велосипед?",bycicle}
   }	;
 
 
 
+
+struct S
+{
+    std::wstring a;
+    std::wstring b;
+};
+
+const std::array<std::array<S, 5>, 6> aaa2 = {
+  {
+    {
+      {
+{L"1 ракушек", L"1 ракушку"},   { L"2 ракушек", L"2 ракушки" }, { L"3 ракушек", L"3 ракушки" }, { L"4 ракушек", L"4 ракушки" }, { L"21 ракушек", L"21 ракушки" }
+      }
+    },
+    {
+      {
+{L"1 ягод", L"1 ягода"},   { L"2 ягод", L"2 ягоды" }, { L"3 ягод", L"3 ягоды" }, { L"4 ягод", L"4 ягоды" }, { L"21 ягод", L"21 ягода" }
+      }
+    },
+    
+    {
+      {
+{L"1 грибов", L"1 гриб"},   { L"2 грибов", L"2 гриба" }, { L"3 грибов", L"3 гриба" }, { L"4 грибов", L"4 гриба" }, { L"21 грибов", L"21 гриб" }
+      }
+    }
+
+    ,
+
+
+    {
+      {
+{L"1 рыбок", L"1 рыбку"},   { L"2 рыбок", L"2 рыбки" }, { L"3 рыбок", L"3 рыбки" }, { L"4 рыбок", L"4 рыбки" }, { L"21 рыбок", L"21 рыбку" }
+      }
+    }
+,
+
+        {
+      {
+{L"1 рыбочек", L"1 бабочку"},   { L"2 рыбочек", L"2 бабочки" }, { L"3 рыбочек", L"3 бабочки" }, { L"4 рыбочек", L"4 бабочки" }, { L"21 бабочек", L"21 бабочку" }
+      }
+    }
+
+    ,
+
+            {
+      {
+{L"1 рублей", L"1 рубль"},   { L"2 рублей", L"2 рубля" }, { L"3 рублей", L"3 рубля" }, { L"4 рублей", L"4 рубля" }, { L"5 рублей", L"21 рубль" }
+      }
+    }
+
+
+  }
+};
+
+const std::array <std::array<int, X>, Y> INT0 = { 
+    5,7,3,1,1,
+    5,7,3,1,1,
+    5,7,3,1,1,
+    5,7,3,1,1,
+    5,7,3,1,1,
+    5,7,3,1,1
+};
 
 const std::wstring CommentsDic[] =
 {
@@ -31,77 +94,3 @@ const std::wstring CommentsDic[] =
 
 
 
-//const std::vector<pica> flower = {
-//  // type   r   x0 y0   x1y1 x2y2   FillColor               outlineColor       OutlineThickness    angle
-//  {circle, 30, 148,125, 0,0, 0,0, color::aqua, sf::Color(0,0,0), Settings::THICKNESS,   2.0f, 0, 0 },
-//  {circle, 30, 21,20,   0,0, 0,0, color::red, sf::Color(0,0,0),  Settings::THICKNESS,   2.0f, 0, 0 },
-//  {circle, 30, 148,20,  0,0, 0,0, color::coral, sf::Color(0,0,0),  Settings::THICKNESS,   2.0f, 0, 0 },
-//  {circle, 30, 18,125,  0,0, 0,0, sf::Color(181, 72, 213), sf::Color(0,0,0), Settings::THICKNESS,   2.0f, 0, 0 },
-//  {circle, 45, 70,57,   0,0, 0,0, sf::Color(255, 220, 156), sf::Color(0,0,0),Settings::THICKNESS,   2.0f, 0, 0 },
-//  
-//  {triangle,10,0.0f,230.0f,80.0f,230.0f,40.0f,260.0f, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS, 0, 0, 0},
-//
-//  {triangle,10,140.0f,230.0f,220.0f,230.0f,180.0f,260.0f, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS, 0, 0, 0},
-//
-//  {rectangle,0,105,160,0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,10,180}
-//};
-//
-//
-//
-//const std::vector<pica> tree = {
-//
-//
-//    {circle, 10, 180,122, 0,0, 0,0, sf::Color(255, 96, 114), sf::Color(0,0,0), Settings::THICKNESS,   2.0f },
-//    {circle, 10, 230,122,   0,0, 0,0, sf::Color(107, 177, 0), sf::Color(0,0,0),  Settings::THICKNESS,   2.0f },
-//    {circle, 10, 280,122,  0,0, 0,0, sf::Color(107, 177, 0), sf::Color(0,0,0),  Settings::THICKNESS,   2.0f },
-//    {rectangle,0,100,110,0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,220,50},
-//    {rectangle,0,250,170,0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,20,90},
-//    {rectangle,0,250,8,0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,20,90},
-//    {triangle,0,330.0f,114.0f,    364.0f,132.0f,    330.0f,155.0f,    sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//    {triangle,12,361,30,393,30,378,96.0f, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//
-//    {triangle,12,361,230,393,230,378,164.0f, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//
-//    {triangle,12,0,45,85,45,85,169.0f, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS}
-//
-//
-//};
-//
-//float m1 = 130;
-//const std::vector<pica> tower = {
-//    // type   r   x0 y0   x1y1 x2y2   FillColor               outlineColor       OutlineThickness    angle
-//        {circle, 30, 201,90,  0,0, 0,0, sf::Color(107, 177, 0), sf::Color(0,0,0),  Settings::THICKNESS,   2.0f },
-//        {rectangle,0,50,60,0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,100,140},
-//        {rectangle,0,50 + m1,60,  0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,100,140},
-//        {rectangle,0,50 + 2 * m1,60,  0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,100,140},
-//        //{rectangle,0,50,60,0,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,100,160},
-//
-//        {triangle,10,  51,56,  149,56,  103,0, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//        {triangle,10,  51 + m1,56,  149 + m1,56,  103 + m1,0, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//        {triangle,10,  51 + 2 * m1,56,  149 + 2 * m1,56,  103 + 2 * m1,0, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//
-//
-//
-//        //{triangle,10,  59,48,  143,48,  104,0, sf::Color(253, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS}
-//};
-//
-//
-//
-//
-//
-//const std::vector<pica> car = {
-//
-//    {rectangle,0,60,110,0 ,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,250,60},
-//    {rectangle,0,100,50,0 ,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,170,50},
-//    {triangle,10,  60,110,  90,110,     60  ,140, sf::Color(60, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//    {triangle,10,  310,110,  310,140,     280  ,110, sf::Color(60, 255, 5) , sf::Color(0,0,0) ,Settings::THICKNESS},
-//    {circle,35,57 ,184,0,0,0,0,sf::Color(255, 220, 156) , sf::Color(0,0,0) ,Settings::THICKNESS,   0 },
-//    {circle,35,245 ,184,0,0,0,0,sf::Color(255, 220, 156) , sf::Color(0,0,0) ,Settings::THICKNESS,   0 },
-//
-//    {circle,10,80 ,210,0,0,0,0,sf::Color(255, 220, 156) , sf::Color(0,0,0) ,Settings::THICKNESS,   0 },
-//    {circle,10,269 ,210,0,0,0,0,sf::Color(255, 220, 156) , sf::Color(0,0,0) ,Settings::THICKNESS,   0},
-//
-//    {rectangle,0,240,69,0 ,0,0,0,sf::Color(42, 170, 205) , sf::Color(0,0,0) ,Settings::THICKNESS,   0,30,30},
-//    {circle,7,248 ,77,0,0,0,0,sf::Color(255, 220, 156) , sf::Color(0,0,0) ,Settings::THICKNESS,   0}
-//
-//};
