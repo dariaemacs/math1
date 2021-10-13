e860836ed96f33c9b964ce2a32308fbe15e00e8f
andron81 <andron81_81@mail.ru>
2021-10-13 22:13:03 +0300

    quest_14
    quest_14

--- include/window.hpp
+++ include/window.hpp
@@ -765,4 +765,22 @@ public:
     int getquestionVariantID5() { return questionVariantID5; }
 };
 
+class QuestType14 : public Window {
+
+    int questionVariantID1;
+    int questionVariantID2;
+    int questionVariantID3;
+    int questionVariantID4;
+    int questionVariantID5;
+    questanswerClass questanswer;
+    CheckButton11 checkbutton;
+    table3_5 tab;
+public:
+    QuestType14(int, int, int);
+    int getquestionVariantID1() { return questionVariantID1; }
+    int getquestionVariantID2() { return questionVariantID2; }
+    int getquestionVariantID3() { return questionVariantID3; }
+    int getquestionVariantID4() { return questionVariantID4; }
+    int getquestionVariantID5() { return questionVariantID5; }
+};
 #endif 
--- src/window.cpp
+++ src/window.cpp
@@ -3873,3 +3873,121 @@ QuestType16::QuestType16(int w, int h, int qtyButtons) :
 
 
 }
+
+QuestType14::QuestType14(int w, int h, int qtyButtons) :
+    questionVariantID1(rand() % 3),
+    questionVariantID2(rand() % 3),
+    Window(w, h, 0, 13),
+    tab(*this) ,
+    checkbutton(*this)
+        {
+
+    bool first = true;
+
+    CheckButtonTexture.loadFromFile("resources/images/arrow_disable.png");
+    CheckButtonSprite.setTexture(CheckButtonTexture);
+    sf::Event event;
+    //questanswer[0].loadFromFile("resources/images/arrow_disable.png");
+
+    textFrame.settext(question14Text[0].questionText + L" " + question14Text1[questionVariantID1] + L"\n" +
+        question14Text2[questionVariantID1][2]
+    );
+    textFrame.CalcucateCoordinate(w - w * 10 / 100, h / 3);
+
+
+    while (window->isOpen()) {
+        window->clear();
+
+        window->draw(List);
+
+        if (first) {
+
+            first = false;
+
+            //buttons.CalcucateCoordinate((h - 100) / 1.4);
+
+            QuestComment.setmargin_top(100);
+            QuestComment.CalcucateCoordinate(h / 3, w / 2);
+
+
+
+
+
+
+
+
+        }
+
+        tab.draw();
+        window->draw(QuestComment.gettext());
+        window->draw(textFrame.gettext());
+ 
+        window->draw(CheckButtonSprite);
+
+
+        if (badAnswer) {
+            sf::Sprite sprite(questanswer.getminiwindow().getTexture());
+            //sprite.setPosition((w - questanswer.getWidth()) / 2, buttons.getMarginTop() - 2 * questanswer.getWidth() * questanswer.getScale());
+            sprite.setPosition(0,0);
+            window->draw(sprite);
+
+        }
+        window->display();
+        while (window->pollEvent(event)) {
+            if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed) {
+                window->close();
+            }
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+
+                //if (readyforCheck && checkandnextQuest(Settings::ButtonFactor))
+                    /*if (question15Answer[questionVariantID1] == buttons.GetButtonsClickID() + 1) QuestComment.settext(CommentsDic[13]);
+                    else {
+                        QuestComment.settext(CommentsDic[2]);
+                        badAnswer = true;
+                        questanswer.setParams(buttons.getWidth() * 5, buttons.getHeight(), 5, buttons.getScale());
+                        int a = atoi(question14DigitsInTable[questionVariantID1][0][question16AnswerString[questionVariantID1]].c_str());
+                        int b = atoi(question14DigitsInTable[questionVariantID1][1][question16AnswerString[questionVariantID1]].c_str());
+                        if (a > b) {
+                            questanswer[0].loadFromFile(res_path + "digit" + std::to_string(a) + ".jpg");
+                            questanswer[2].loadFromFile(res_path + "digit" + std::to_string(b) + ".jpg");
+                        }
+                        else
+                        {
+                            questanswer[0].loadFromFile(res_path + "digit" + std::to_string(b) + ".jpg");
+                            questanswer[2].loadFromFile(res_path + "digit" + std::to_string(a) + ".jpg");
+                        }
+
+                        questanswer[1].loadFromFile(res_path + "digit_minus.jpg");
+
+                        questanswer[3].loadFromFile(res_path + "digit_equal.jpg");
+                        questanswer[4].loadFromFile(res_path + "digit" + std::to_string(
+                            question16Answer[questionVariantID1]
+
+                        ) + ".jpg");
+
+                        buttons.getButtonTexture()[buttons.GetButtonsClickID()]->loadFromFile(
+                            "resources/images/digit" + std::to_string(buttons.GetButtonsClickID() + 1) + "_wrong.jpg");
+
+                        buttons.getButtonTexture()[question15Answer[questionVariantID1] - 1]->loadFromFile(
+                            "resources/images/digit" + std::to_string(question15Answer[questionVariantID1]) + "_right.jpg"
+                        );
+
+                        questanswer.draw();
+                    }*/
+
+             /*   if (buttons.click()) {
+                    CheckButtonTexture.loadFromFile("resources/images/arrow_up.png"); readyforCheck = true;
+                    CheckButtonSprite.setTexture(CheckButtonTexture);
+                }
+*/
+            }
+
+
+        }
+    }
+
+    srand(time(0));
+
+
+}
